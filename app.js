fa475f3fc7ce84343e39b7e9b0f36da7c39364c4
sanjay59 <sanjayyadav224210@gmail.com>
2024-10-17 12:33:14 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app  = express();
+
+app.get('/',(req,res)=>{
+    res.send("Home page");
+})
+
+
+let PORT = process.env.PORT || 3000
+app.listen(PORT, () =>{
+    console.log(`Server is running on port `+PORT);
+})
+
